2374ba65909c53150452f33757d5c2341b258593
Ruhul Amin Parvez <a@Ruhul-Amin-Parvezs-MacBook-Pro.local>
2023-11-04 19:22:24 +0600

    Added Overlay Effect

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,7 +1,21 @@
+import { useState, useRef } from 'react';
 import imgData from './../../utils/imgData';
 import { FaImage } from 'react-icons/fa';
 
 const Gallery = () => {
+    const [uploadedImage, setUploadedImage] = useState(null);
+    const inputRef = useRef();
+
+    console.log(uploadedImage);
+
+    const handleImageUpload = (e) => {
+        const file = e.target.files[0];
+        setUploadedImage(file);
+    };
+    const handleContainerClick = () => {
+        inputRef.current.click();
+    };
+
     return (
         <div className="g-container">
             <div className="g-upper">
@@ -20,9 +34,19 @@ const Gallery = () => {
                                 </div>
                             )
                         })}
-                        <div className="g-upload-container">
+                        <div className="g-upload-container" onClick={handleContainerClick}>
                             <div className="g-upload">
-                                <FaImage className='g-up-icon' />
+                                <input
+                                    type="file"
+                                    id="imageUpload"
+                                    accept="image/*"
+                                    onChange={handleImageUpload}
+                                    style={{ display: 'none' }}
+                                    ref={inputRef}
+                                />
+                                <label htmlFor="imageUpload">
+                                    <FaImage className='g-up-icon' />
+                                </label>
                             </div>
                             <p className='g-add-img'>Add Images</p>
                         </div>
